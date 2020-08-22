e6d854403b37a52bc6042b2bfa6b9ed05f4245c4
Nunu-0 <66766470+Nunu-0@users.noreply.github.com>
2020-08-23 00:53:08 +0900

    별 찍기 - 13

---
+++ Baek.J/2523.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	cin >> a;
+	for (int i = 0; i < a; i++) {
+		for (int j = 0; j <= i; j++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+	for (int i = 0; i < a - 1; i++) {
+		for (int j = a - 1; j > i; j--) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+	return 0;
+}
