ee32a7e1d3e4dab7f228784605ef26383f8842e9
Nunu-0 <66766470+Nunu-0@users.noreply.github.com>
2020-08-23 04:09:24 +0900

    별찍기 - 9

--- Baek.J/2446.cpp
+++ Baek.J/2446.cpp
@@ -5,10 +5,10 @@ int main() {
 	int n;
 	cin >> n;
 	for (int i = 0; i < 2 * n - 1; i++) {
-		for (int j = n - 1; j > abs(i - n + 1); j--) {
+		for (int j = n - 1; j > abs(i - n + 1); j--) { //공백 출력
 			cout << " ";
 		}
-		for (int k = 0; k < 2 * abs(i - n + 1) + 1; k++) {
+		for (int k = 0; k < 2 * abs(i - n + 1) + 1; k++) { //*출력
 			cout << "*";
 		}
 		cout << endl;
