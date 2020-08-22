f4472e7f21f19b46fe545d73c75bf86370367142
Nunu-0 <66766470+Nunu-0@users.noreply.github.com>
2020-08-23 04:00:16 +0900

    별 찍기 - 9

---
+++ Baek.J/2446.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	int star = n ,space = 0;
+	for (int i = 0; i < 2 * n - 1; i++) {
+		for (int j = n - 1; j > abs(i - n + 1); j--) {
+			cout << " ";
+		}
+		for (int k = 0; k < 2 * abs(i - n + 1) + 1; k++) {
+			cout << "*";
+		}
+		cout << endl;
+	}
+	return 0;
+}
