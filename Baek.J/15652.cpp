16c829a38a3741442cd0f9dd7b081824283bf9df
Nunu-0 <66766470+Nunu-0@users.noreply.github.com>
2020-09-12 15:03:03 +0900

    N과 M (4)

---
+++ Baek.J/15652.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+//N과 M (4)
+int N, M;
+vector <int> num;
+
+void solution() {
+	if (num.size() == M) {
+		for (int i = 0; i < M; i++) {
+			cout << num[i] << " ";
+		}
+		cout << "\n";
+	}
+	else {
+		for (int i = 1; i <= N; i++) {
+			if (num.size() == 0 || num[num.size() -1] <= i) {
+				num.push_back(i);
+				solution();
+				num.pop_back();
+			}
+		}
+	}
+}
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> N >> M;
+	solution();
+
+	return 0;
+}
