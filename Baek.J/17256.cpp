260f09ba7174f723bed6586318cb29720423e293
Nunu-0 <66766470+Nunu-0@users.noreply.github.com>
2020-11-24 23:34:37 +0900

    달달함이 넘쳐흘러

---
+++ Baek.J/17256.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+// 달달함이 넘쳐흘러
+int main() {
+	int ax, ay, az, bx, by, bz;
+
+	cin >> ax >> ay >> az >> bx >> by >> bz;
+	cout <<  bx - az << " " << by / ay << " " << bz - ax;
+
+	return 0;
+}
